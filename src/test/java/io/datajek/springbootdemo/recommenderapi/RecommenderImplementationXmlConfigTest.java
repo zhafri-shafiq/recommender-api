8ddd5b6aaeda8250cb286dacb049b95adc9e4538
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-07-06 18:29:57 +0800

    Unit testing for Xml Context

---
+++ src/test/java/io/datajek/springbootdemo/recommenderapi/RecommenderImplementationXmlConfigTest.java
@@ -0,0 +1,23 @@
+package io.datajek.springbootdemo.recommenderapi;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/testContext.xml")
+class RecommenderImplementationXmlConfigTest {
+
+    @Autowired
+    private RecommenderImplementation recommenderImpl;
+
+    @Test
+    void testRecommendMovies() {
+        assertArrayEquals(new String[] {"Happy Feet", "Ice Age", "Shark Tale"},
+                recommenderImpl.recommendMovies("Finding Dory"));
+    }
+}
