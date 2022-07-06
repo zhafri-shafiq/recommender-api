4e30a99c5c46eb69e68cae49ba9e50033866d107
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-07-06 18:37:46 +0800

    Spring unit testing with Mockito

---
+++ src/test/java/io/datajek/springbootdemo/recommenderapi/RecommenderImplementationMockTest.java
@@ -0,0 +1,26 @@
+package io.datajek.springbootdemo.recommenderapi;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class RecommenderImplementationMockTest {
+
+    @Mock
+    private Filter mockFilter;
+
+    @InjectMocks
+    private RecommenderImplementation recommenderImpl;
+
+    @Test
+    void testRecommendMovies_noRecommendationsFound() {
+        when(mockFilter.getRecommendations("Finding Dory")).thenReturn(new String[] {});
+        assertArrayEquals(new String[] {}, recommenderImpl.recommendMovies("Finding Dory"));
+    }
+}
