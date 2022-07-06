3d53866995899b2d6a9ae1d92bc31dfb9a2fcaa5
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-07-06 16:35:48 +0800

    assert methods

--- src/main/java/io/datajek/springbootdemo/recommenderapi/ArrayMethods.java
+++ src/main/java/io/datajek/springbootdemo/recommenderapi/ArrayMethods.java
@@ -17,7 +17,37 @@ public class ArrayMethods {
     }
 
     boolean searchForNumber(int[] array, int numberToSearchFor) {
+        int n = array.length;
+        if (n == 0) {
+            return false;
+        }
+
         boolean found = false;
+        for (int i =0; i < n; i++) {
+            if (array[i] == numberToSearchFor) {
+                found = true;
+                break;
+            }
+        }
         return found;
+    }
+
+    int[] sortArray(int[] array) {
+        int temp = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    void printArray(int[] array, int index) {
+        System.out.println(array[index]);
     }
 }
--- src/test/java/io/datajek/springbootdemo/recommenderapi/ArrayMethodsTest.java
+++ src/test/java/io/datajek/springbootdemo/recommenderapi/ArrayMethodsTest.java
@@ -15,7 +15,7 @@ class ArrayMethodsTest {
         int result = arrayMethods.findIndex(new int[]{8, 4, 5}, 5);
 
         //3. compare the actual results to the expected
-        assertEquals(2, result);
+        assertEquals(2, result, "The findIndex method finds the index of a given number");
     }
 
     @Test
@@ -28,5 +28,47 @@ class ArrayMethodsTest {
     public void testfindIndex_emptyArray() {
         ArrayMethods arrayMethods = new ArrayMethods();
         assertEquals(-1, arrayMethods.findIndex(new int[]{}, 1));
+    }
+
+    @Test
+    public void testsortArray_unsortedArray() {
+        ArrayMethods arrayMethods = new ArrayMethods();
+        assertArrayEquals(new int[] {0,1,3,7}, arrayMethods.sortArray(new int[] {3,1,7,0}));
+    }
+
+    @Test
+    void testAssert_True() {
+        Boolean condition = true;
+        assertEquals(true, condition);
+        assertTrue(condition);
+    }
+
+    @Test
+    void testAssert_Null() {
+        String str = null;
+        assertEquals(null, str);
+        assertNull(str);
+    }
+
+    @Test
+    void testunfinishTest_Fail() {
+        fail("unimplemented methods");
+    }
+
+    @Test
+    public void testfindIndex_indexOutOfBound() {
+        ArrayMethods arrayMethods = new ArrayMethods();
+        assertThrows(ArrayIndexOutOfBoundsException.class, ()->arrayMethods.printArray(
+                new int[] {1,8,5}, 3));
+    }
+
+    @Test
+    public void testfindIndex() {
+        ArrayMethods arrayMethods = new ArrayMethods();
+        assertAll(
+                () -> assertEquals(1, arrayMethods.findIndex(new int[]{8,4,5}, 4)),
+                () -> assertEquals(-1, arrayMethods.findIndex(new int[]{8,4,5}, 1)),
+                () -> assertEquals(-1, arrayMethods.findIndex(new int[]{}, 1))
+        );
     }
 }
